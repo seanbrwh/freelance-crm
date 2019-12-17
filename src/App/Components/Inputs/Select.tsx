f01a5824dfc26a47aaa87ab7c30c0971937d78b2
seanbrwh <seanbrwh@gmail.com>
2019-12-17 07:20:39 -0700

    adding more basic needed components

---
+++ src/App/Components/Inputs/Select.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Select() {
+  return (
+    <div>
+      <select name="" id="">
+        <option value="">default</option>
+      </select>
+    </div>
+  );
+}
