2350a017c2708142e35c1a235f04ebf483ecf246
seanbrwh <seanbrwh@gmail.com>
2019-12-17 20:55:41 -0700

    adding css reset and normalize

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,14 +1,20 @@
 import React from "react";
+import styled from "styled-components";
 import Routes from "./Routes";
 import Navbar from "./Components/Navbar";
 import Footer from "./Components/Footer";
 
+const Main = styled.main`
+  width: 100%;
+  background: grey;
+`;
+
 export default function App() {
   return (
-    <div>
+    <Main>
       <Navbar />
       {Routes}
       <Footer />
-    </div>
+    </Main>
   );
 }
--- src/App/Components/Inputs/Textarea.tsx
+++ src/App/Components/Inputs/Textarea.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Textarea() {
   return (
     <div>
-      <textarea name="" id="" cols="30" rows="10"></textarea>
+      <textarea></textarea>
     </div>
   );
 }
