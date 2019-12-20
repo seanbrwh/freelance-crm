9d6ee7e556d4c533aa57f1253002ce8caf35c3a8
seanbrwh <seanbrwh@gmail.com>
2019-12-19 17:54:44 -0700

    working on ui

--- src/App/Components/Inputs/Button.tsx
+++ src/App/Components/Inputs/Button.tsx
@@ -1,9 +1,14 @@
 import React from "react";
+import styled from "styled-components";
 
-export default function Button() {
+const IButton = styled.input``;
+const Label = styled.label``;
+
+export default function Button({ handleClick, label }) {
   return (
-    <div>
-      <input type="button" />
-    </div>
+    <>
+      <Label htmlFor="button">{label}</Label>
+      <IButton type="button" name="button" onClick={handleClick}></IButton>
+    </>
   );
 }
--- src/App/Components/Inputs/Card.tsx
+++ src/App/Components/Inputs/Card.tsx
@@ -1,5 +1,30 @@
 import React from "react";
+import styled from "styled-components";
+
+const Row = styled.div``;
+const Col = styled.div``;
+const ICard = styled.div``;
+const ImgCard = styled.div``;
+const CardImg = styled.img``;
+const CardTitle = styled.span``;
+const CardContent = styled.div``;
+const CardAction = styled.div``;
 
 export default function Card() {
-  return <div>Card</div>;
+  return (
+    <Row>
+      <Col>
+        <ICard>
+          <ImgCard>
+            <CardImg />
+            <CardTitle>Card Title</CardTitle>
+          </ImgCard>
+          <CardContent>
+            <p>Simple Card</p>
+          </CardContent>
+          <CardAction>some action or link</CardAction>
+        </ICard>
+      </Col>
+    </Row>
+  );
 }
