3fb7ebdc23e800b117cfd02f8106add84678a067
seanbrwh <seanbrwh@gmail.com>
2019-12-20 12:55:42 -0700

    chaning auth0 from class to function

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Router } from "react-router-dom";
 import { Auth0Provider } from "./react-auth0-spa";
-import config from "../../auth_config.json";
 import history from "./utils/history";
 import App from "./App";
 
--- src/App/react-auth0-spa.tsx
+++ src/App/react-auth0-spa.tsx
@@ -1,8 +1,10 @@
-import React, { useState, useEffect, useContext } from "react";
-import createAuth0Client from "@auth0/auth0-spa-js";
+import * as React from "react";
+import auth0Js from "auth0-js";
 import history from "./utils/history";
+let { DOMAIN, CLIENT_ID, AUTH_CALLBACK } = process.env;
 
-interface AUTH0PROVIDER {
+const { useState, createContext, useContext } = React;
+interface Auth0Props {
   children: any;
   onRedirectCallback: any;
   domain: any;
@@ -11,86 +13,235 @@ interface AUTH0PROVIDER {
   audience: any;
 }
 
-const DEFAULT_REDIRECT_CALLBACK = () =>
-  window.history.replaceState({}, document.title, window.location.pathname);
-
 export const Auth0Context = React.createContext(null);
 export const useAuth0 = () => useContext(Auth0Context);
-export const Auth0Provider = ({
-  children,
-  onRedirectCallback = DEFAULT_REDIRECT_CALLBACK,
-  ...initOptions
-}: AUTH0PROVIDER) => {
-  const [isAuthenticated, setIsAuthenticated] = useState();
+
+export const Auth0Provider = ({ children, ...initOptions }: Auth0Props) => {
+  const auth0 = new auth0Js.WebAuth({
+    domain: DOMAIN,
+    clientID: CLIENT_ID,
+    redirectUri: AUTH_CALLBACK,
+    responseType: "token id_token",
+    scope: "openid profile email"
+  });
   const [user, setUser] = useState();
   const [auth0Client, setAuth0] = useState();
   const [loading, setLoading] = useState(true);
   const [popupOpen, setPopupOpen] = useState(false);
 
-  useEffect(() => {
-    const initAuth0 = async () => {
-      const auth0FromHook = await createAuth0Client(initOptions);
-      setAuth0(auth0FromHook);
+  const googleLogin = () => {
+    auth0.authorize({ connection: "google" }, (err, authResult) => {});
+  };
 
-      if (window.location.search.includes("code=")) {
-        const { appState } = await auth0FromHook.handleRedirectCallback();
-        onRedirectCallback(appState);
-      }
+  const facebookLogin = () => {
+    auth0.authorize({ connection: "facebook" }, (err, authResult) => {});
+  };
 
-      const isAuthenticated = await auth0FromHook.isAuthenticated();
+  const githubLogin = () => {
+    auth0.authorize({ connection: "github" }, (err, authResult) => {});
+  };
 
-      setIsAuthenticated(isAuthenticated);
+  const linkedinLogin = () => {
+    auth0.authorize({ connection: "linkedin" }, (err, authResult) => {});
+  };
 
-      if (isAuthenticated) {
-        const user = await auth0FromHook.getUser();
-        setUser(user);
-      }
+  const traditionalLogin = (email, username, password) => {
+    auth0.login(
+      { realm: "test", email: "", username: "", password: "" },
+      err => {}
+    );
+  };
+  const passwordlessStart = email => {
+    auth0.passwordlessStart(
+      {
+        connection: "email",
+        send: "code",
+        email: email
+      },
+      (err, res) => {}
+    );
+  };
 
-      setLoading(false);
-    };
-    initAuth0();
-    // eslint-disable-next-line
-  }, []);
+  const passwordlessLogin = email => {
+    auth0.passwordlessLogin(
+      {
+        connection: "email",
+        email: email,
+        verificationCode: ""
+      },
+      (err, res) => {}
+    );
+  };
 
-  const loginWithPopup = async (params = {}) => {
-    setPopupOpen(true);
-    try {
-      await auth0Client.loginWithPopup(params);
-    } catch (error) {
-      console.error(error);
-    } finally {
-      setPopupOpen(false);
+  const signUp = (email, pass) => {
+    auth0.signup(
+      {
+        connection: "Username-Password-Authentication",
+        email: email,
+        password: pass
+      },
+      err => {}
+    );
+  };
+
+  const passwordReset = () => {
+    auth0.changePassword(
+      {
+        connection: "",
+        email: ""
+      },
+      (err, resp) => {}
+    );
+  };
+  const tokenRenewal = () => {
+    auth0.checkSession({}, (err, authResult) => {});
+  };
+
+  const logout = () => {
+    // Clear access token and ID token from local storage
+    localStorage.removeItem("access_token");
+    localStorage.removeItem("id_token");
+    localStorage.removeItem("expires_at");
+    localStorage.removeItem("user");
+
+    history.replace("/");
+  };
+
+  const handleAuthentication = () => {
+    if (typeof window !== "undefined") {
+      auth0.parseHash({ hash: window.location.hash }, (err, authResult) => {
+        if (authResult && authResult.accessToken && authResult.idToken) {
+          setSession(authResult);
+          history.replace("/");
+        } else if (err) {
+          history.replace("/");
+          console.log(err);
+        }
+      });
     }
-    const user = await auth0Client.getUser();
-    setUser(user);
-    setIsAuthenticated(true);
   };
 
-  const handleRedirectCallback = async () => {
-    setLoading(true);
-    await auth0Client.handleRedirectCallback();
-    const user = await auth0Client.getUser();
-    setLoading(false);
-    setIsAuthenticated(true);
-    setUser(user);
+  const isAuthenticated = () => {
+    let expiresAt = JSON.parse(localStorage.getItem("expires_at"));
+    return new Date().getTime() < expiresAt;
   };
-  return (
-    <Auth0Context.Provider
-      value={{
-        isAuthenticated,
-        user,
-        loading,
-        popupOpen,
-        loginWithPopup,
-        handleRedirectCallback,
-        getIdTokenClaims: (...p: any[]) => auth0Client.getIdTokenClaims(...p),
-        loginWithRedirect: (...p: any[]) => auth0Client.loginWithRedirect(...p),
-        getTokenSilently: (...p: any[]) => auth0Client.getTokenSilently(...p),
-        getTokenWithPopup: (...p: any[]) => auth0Client.getTokenWithPopup(...p),
-        logout: (...p: any[]) => auth0Client.logout(...p)
-      }}
-    >
-      {children}
-    </Auth0Context.Provider>
-  );
+
+  const setSession = authResult => {
+    const expiresAt = JSON.stringify(
+      authResult.expiresIn * 1000 + new Date().getTime()
+    );
+    localStorage.setItem("access_token", authResult.accessToken);
+    localStorage.setItem("id_token", authResult.idToken);
+    localStorage.setItem("expires_at", expiresAt);
+    auth0.client.userInfo(authResult.accessToken, (err, user) => {
+      localStorage.setItem("user", JSON.stringify(user));
+      history.replace("/");
+    });
+  };
+
+  const getUser = () => {
+    if (localStorage.getItem("user")) {
+      return JSON.parse(localStorage.getItem("user"));
+    }
+  };
+
+  const getUserName = () => {
+    if (getUser()) {
+      return getUser().name;
+    }
+  };
+
+  const getAccessToken = () => {
+    const accessToken = localStorage.getItem("access_token");
+    if (!accessToken) {
+      throw new Error("No access token found");
+    }
+    return accessToken;
+  };
+
+  return <Auth0Context.Provider value={{}}>{children}</Auth0Context.Provider>;
 };
+
+// const DEFAULT_REDIRECT_CALLBACK = () =>
+//   window.history.replaceState({}, document.title, window.location.pathname);
+
+// export const Auth0Context = React.createContext(null);
+// export const useAuth0 = () => useContext(Auth0Context);
+// export const Auth0Provider = ({
+//   children,
+//   onRedirectCallback = DEFAULT_REDIRECT_CALLBACK,
+//   ...initOptions
+// }: AUTH0PROVIDER) => {
+//   const [isAuthenticated, setIsAuthenticated] = useState();
+//   const [user, setUser] = useState();
+//   const [auth0Client, setAuth0] = useState();
+//   const [loading, setLoading] = useState(true);
+//   const [popupOpen, setPopupOpen] = useState(false);
+
+//   useEffect(() => {
+//     const initAuth0 = async () => {
+//       const auth0FromHook = await createAuth0Client(initOptions);
+//       setAuth0(auth0FromHook);
+
+//       if (window.location.search.includes("code=")) {
+//         const { appState } = await auth0FromHook.handleRedirectCallback();
+//         onRedirectCallback(appState);
+//       }
+
+//       const isAuthenticated = await auth0FromHook.isAuthenticated();
+
+//       setIsAuthenticated(isAuthenticated);
+
+//       if (isAuthenticated) {
+//         const user = await auth0FromHook.getUser();
+//         setUser(user);
+//       }
+
+//       setLoading(false);
+//     };
+//     initAuth0();
+//     // eslint-disable-next-line
+//   }, []);
+
+//   const loginWithPopup = async (params = {}) => {
+//     setPopupOpen(true);
+//     try {
+//       await auth0Client.loginWithPopup(params);
+//     } catch (error) {
+//       console.error(error);
+//     } finally {
+//       setPopupOpen(false);
+//     }
+//     const user = await auth0Client.getUser();
+//     setUser(user);
+//     setIsAuthenticated(true);
+//   };
+
+//   const handleRedirectCallback = async () => {
+//     setLoading(true);
+//     await auth0Client.handleRedirectCallback();
+//     const user = await auth0Client.getUser();
+//     setLoading(false);
+//     setIsAuthenticated(true);
+//     setUser(user);
+//   };
+//   return (
+//     <Auth0Context.Provider
+//       value={{
+//         isAuthenticated,
+//         user,
+//         loading,
+//         popupOpen,
+//         loginWithPopup,
+//         handleRedirectCallback,
+//         getIdTokenClaims: (...p: any[]) => auth0Client.getIdTokenClaims(...p),
+//         loginWithRedirect: (...p: any[]) => auth0Client.loginWithRedirect(...p),
+//         getTokenSilently: (...p: any[]) => auth0Client.getTokenSilently(...p),
+//         getTokenWithPopup: (...p: any[]) => auth0Client.getTokenWithPopup(...p),
+//         logout: (...p: any[]) => auth0Client.logout(...p)
+//       }}
+//     >
+//       {children}
+//     </Auth0Context.Provider>
+//   );
+// };
